c4f6bb13b62ed440d964dad19e153172df2ba0e4
Nastya Nastya <x-nastya2007@yandex.ru>
2026-07-02 17:25:05 +0300

    добавила строки с добавлением продуктов

--- Task 3/Schema.sql
+++ Task 3/Schema.sql
@@ -3,3 +3,7 @@ create table Product (
 	name char(20) not null,
 	price integer check(price >= 0)
 );
+
+insert into Product (name, price) values ('milk', 100);
+insert into Product (name, price) values ('coffee', 80);
+insert into Product (name, price) values ('tea', 200);
